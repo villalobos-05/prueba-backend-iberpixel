f29f75e825a52295a03d9b301b6d5a72909ab35a
Daniel <danielvillalobosfdz@gmail.com>
2025-01-28 08:44:25 +0100

    Add books router

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from .routers import localBooks
+from .routers import localBooks, books
 
 app = FastAPI()
 app.include_router(localBooks.router)
+app.include_router(books.router)
