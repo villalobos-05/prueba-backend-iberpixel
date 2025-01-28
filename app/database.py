5369aa6489b4e3c148ed75849a18e2effe19aaf9
Daniel <danielvillalobosfdz@gmail.com>
2025-01-28 08:36:26 +0100

    Create mongo database

---
+++ app/database.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
+
+# Load .env variables
+load_dotenv()
+
+
+def get_database():
+    # Create a connection
+    client = AsyncIOMotorClient(os.getenv("MONGODB_URI"))
+
+    # Return collection
+    return client[os.getenv("MONGODB_DATABASE")]
+
+
+if __name__ == "__main__":
+    database = get_database()
+    print("Database connected: ", database)
