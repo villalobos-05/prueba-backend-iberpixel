872d50c7fed7ee0fe18f633565ff8ab9dcd8104c
Daniel <danielvillalobosfdz@gmail.com>
2025-01-28 08:54:04 +0100

    Create GET book by id endpoint

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, status, Depends
 from app.models.book import Book
-from app.schemas.book import books_schema
+from app.schemas.book import book_schema, books_schema
+from app.utils.getObjectId import getObjectId
 from app.database import get_database
 
 router = APIRouter(
@@ -23,3 +25,15 @@ async def get_books(author: str | None = None, title: str | None = None):
 
     books = await collection.find(query).to_list()
     return books_schema(books)
+
+
+@router.get("/books/{id}", response_model=Book)
+async def get_book_by_id(id: Annotated[str, Depends(getObjectId)]):
+    book = await collection.find_one({"_id": id})
+
+    if not book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with id {id} not found"
+        )
+
+    return book_schema(book)
